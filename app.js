11d8e0af9c3a88a165777665c8c30429a33b6c8b
ADMEC <info@admecindia.co.in>
2024-01-16 11:47:20 +0530

    first, moving to git

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const express = require( "express" );
+const express = require("express");
 // const bodyparser = require( "body-parser" );
-const cors = require( "cors" );
+const cors = require("cors");
+require("./config/database");
 
-require( "./config/database" );
-const hb = require( "./config/handlebars" )
-const users = require( "./routes/users" )
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+const hb = require("./config/handlebars")
+const users = require("./routes/users")
 
 const app = express();
 
 //to get the data from form
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 //set template engine
 app.engine("hbs", hb);
-app.set("view engine","hbs");
+app.set("view engine", "hbs");
 
 //make way for some custom css, js and images
 app.use('/custom/css', express.static(__dirname + '/views/static/css'));
